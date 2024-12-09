b022aa9f7aa64f485b2049851f1b8a749e29fbd8
SIMKCN <skmarschall@gmail.com>
2024-12-09 19:52:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import filepick from "@/components/AppFilePick.vue"
+export default {
+  name: "app",
+  data() {
+    return {
+      test: "hallo"
+    }
+  },
+  components: {
+    filepick
+  }
+}
+
+</script>
+<template>
+  <div>
+    <filepick></filepick>
+    
+  </div>
+
+
+</template>
---
+++ src/components/AppFilePick.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="file" multiple>
+    <p>{{ file }}</p>
+</template>
+
+<script>
+export default {
+    name: "filepick",
+    data() {
+        return {
+            file: null
+        }
+    }
+
+}
+
+</script>
